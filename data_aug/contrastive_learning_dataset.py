6d5d790a2883813fcc69435dac0ad2df7fb6e99a
George Kaceli <georgekaceli8@gmail.com>
2025-03-06 13:46:39 -0500

    adding project files

---
+++ data_aug/contrastive_learning_dataset.py
@@ -0,0 +1,50 @@
+from torchvision import transforms, datasets
+from data_aug.view_generator import ContrastiveLearningViewGenerator
+from exceptions.exceptions import InvalidDatasetSelection
+
+
+class ContrastiveLearningDataset:
+    def __init__(self, root_folder):
+        self.root_folder = root_folder
+
+    @staticmethod
+    def get_simclr_pipeline_transform(size, s=1):
+        """Return a set of data augmentation transformations as described in the SimCLR paper."""
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        data_transforms = transforms.Compose([
+            transforms.RandomResizedCrop(size=size),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomApply([color_jitter], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.GaussianBlur(kernel_size=int(0.1 * size), sigma=(0.1, 2.0)),
+            transforms.ToTensor()
+        ])
+        return data_transforms
+
+    def get_dataset(self, name, n_views):
+        valid_datasets = {
+            'cifar10': get_cifar10_dataset,
+            'stl10': get_stl10_dataset
+        }
+        try:
+            dataset_fn = valid_datasets[name]
+        except KeyError:
+            raise InvalidDatasetSelection()
+        else:
+            return dataset_fn(self.root_folder, n_views)
+
+
+def get_cifar10_dataset(root_folder, n_views):
+    transform = ContrastiveLearningViewGenerator(
+        ContrastiveLearningDataset.get_simclr_pipeline_transform(32),
+        n_views
+    )
+    return datasets.CIFAR10(root_folder, train=True, transform=transform, download=True)
+
+
+def get_stl10_dataset(root_folder, n_views):
+    transform = ContrastiveLearningViewGenerator(
+        ContrastiveLearningDataset.get_simclr_pipeline_transform(96),
+        n_views
+    )
+    return datasets.STL10(root_folder, split='unlabeled', transform=transform, download=False)
